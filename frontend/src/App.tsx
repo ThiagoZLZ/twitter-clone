54a93143c27b38179fd60f9a45cf499d10591c47
Thiago Carvalho <thiago.carvalho1991@hotmail.com>
2026-06-01 22:31:21 -0300

    add vercel analyts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register/Register";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/profile/:id" element={<Profile />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
